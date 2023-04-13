25e87e9d87b251ac37e0c45a457e02767975c23b
noah figueras <noahfigueras@gmail.com>
2023-04-13 14:12:43 -0700

    @natspec comments for PoolGovernance contract

--- src/PoolGovernance.sol
+++ src/PoolGovernance.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.16;
 
-/**
- * @title Smoothing Pool Governance Contract.
- * @notice This contract is in charged of recieving votes from smoothly validator 
- * nodes with the respective withdrawals, exits and state root hashes of the 
- * computed state for every epoch. Reach consensus and pass the data to the
- * SmoothlyPool contract.
- */
+/// @title Smoothing Pool Governance Contract
+/// @notice This contract is in charge of recieving votes from operator 
+/// nodes with the respective withdrawals, exits and state root hashes of the 
+/// computed state for every epoch. Reach consensus and pass the data to the
+/// SmoothlyPool contract.
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import './SmoothlyPoolV2.sol';
@@ -21,6 +19,11 @@ contract PoolGovernance is Ownable {
   address[] public operators;
   SmoothlyPoolV2 immutable pool;
 
+  /// @notice Epoch data to update the Smoothly Pool state
+  /// @param withdrwals Merkle root hash for withdrawals
+  /// @param exits Merkle root hash for exits 
+  /// @param state MPT root hash of entire state 
+  /// @param fee distributed to operators to keep network alive 
   struct Epoch {
     bytes32 withdrawals;
     bytes32 exits; 
@@ -28,19 +31,24 @@ contract PoolGovernance is Ownable {
     uint fee;
   }
 
+  /// @dev checks if operator is active 
   mapping(address => bool) public isOperator;
+  /// @dev records operator accumulative rewards 
   mapping(address => uint) public operatorRewards;
+  /// @dev records operator votes for each epochNumber
   mapping(uint => mapping(address => Epoch)) public votes;
 
   error ExistingOperator(address operator);
   error Unauthorized();
   error EpochTimelockNotReached();
 
+  /// @dev restrict calls only to operators
   modifier onlyOperator {
     if(!isOperator[msg.sender]) revert Unauthorized();
     _;
   }
 
+  /// @dev restrict calls only to Smoothly Pool
   modifier onlyPool {
     if(msg.sender != address(pool)) revert Unauthorized();
     _;
@@ -51,17 +59,16 @@ contract PoolGovernance is Ownable {
     pool = SmoothlyPoolV2(_pool);
   }
 
+  /// @dev Recieves fees from Smoothly Pool
   receive () onlyPool external payable {
   }
 
-  function getRewards(address operator) public view returns(uint) {
-    return operatorRewards[operator];
-  }
-
+  /// @notice Gets all active operators 
   function getOperators() external view returns(address[] memory) {
     return operators;
   }
 
+  /// @notice withdraws accumulated rewards from an operator
   function withdrawRewards() onlyOperator external {
     address operator = msg.sender;
     uint rewards = getRewards(operator);
@@ -69,6 +76,11 @@ contract PoolGovernance is Ownable {
     _transfer(operator, rewards);
   }
 
+  /// @notice Proposal Data for current epoch computed from every operator
+  /// @dev operators need to reach an agreement of at least votingRatio
+  /// and no penalties are added for bad proposals or no proposals as admin
+  /// have the abilities to delete malicious operators
+  /// @param epoch Data needed to update Smoothly Pool state
   function proposeEpoch(Epoch memory epoch) onlyOperator external {
     if(block.timestamp < (lastEpoch + epochInterval)) revert EpochTimelockNotReached();
     votes[epochNumber][msg.sender] = epoch;
@@ -80,7 +92,6 @@ contract PoolGovernance is Ownable {
         count += 1;
       }
       if(_computeAgreements(count) >= votingRatio) {
-        // Call smoothly pool make sure it doesn't revert
         pool.updateEpoch(
           epoch.withdrawals,
           epoch.exits,
@@ -95,14 +106,18 @@ contract PoolGovernance is Ownable {
     }
   }
 
+  /// @notice Adds operators
+  /// @param _operators List of new operators
   function addOperators(address[] memory _operators) onlyOwner external {
     for (uint i = 0; i < _operators.length; i++){
       if(isOperator[_operators[i]]) revert ExistingOperator(_operators[i]);
-        isOperator[_operators[i]] = true;
-        operators.push(_operators[i]);
+      isOperator[_operators[i]] = true;
+      operators.push(_operators[i]);
     }
   }
 
+  /// @notice Deletes operators
+  /// @param _operators List of operators to be removed
   function deleteOperators(address[] memory _operators) onlyOwner external {
     for (uint i = 0; i < _operators.length; i++){
       isOperator[_operators[i]] = false;
@@ -110,10 +125,21 @@ contract PoolGovernance is Ownable {
     }
   }
 
+  /// @notice Gets cumulative rewards from a single operator
+  /// @param operator address of operator 
+  function getRewards(address operator) public view returns(uint) {
+    return operatorRewards[operator];
+  }
+
+  /// @dev Computes votingRatio
+  /// @param count agreements of all operator up to date 
   function _computeAgreements(uint count) private view returns(uint) {
     return (count * 100) / operators.length; 
   }
 
+  /// @dev Compare to votes 
+  /// @param vote1 Epoch data of other operator 
+  /// @param vote2 Epoch data of caller 
   function _isVoteEqual(Epoch memory vote1, Epoch memory vote2) private pure returns(bool) {
     if(keccak256(abi.encode(vote1)) == keccak256(abi.encode(vote2))){
       return true;
@@ -121,6 +147,9 @@ contract PoolGovernance is Ownable {
     return false;
   }
 
+  /// @dev Distributes rewards equally for all active operators 
+  /// @param fee coming from Smoothly Pool for operators
+  /// @param _operators active 
   function _distributeRewards(uint fee, address[] memory _operators) private {
     uint operatorShare = fee / _operators.length;
     for(uint i = 0; i < _operators.length; i++) {
@@ -128,6 +157,9 @@ contract PoolGovernance is Ownable {
     } 
   }
 
+  /// @dev Utility to remove an operator from operators array without 
+  /// empty space
+  /// @param operator address of operator 
   function _remove(address operator) private {
     address[] memory _operators = operators;
     for (uint i = 0; i < _operators.length; i++){
@@ -140,6 +172,8 @@ contract PoolGovernance is Ownable {
   }
 
   /// @dev Utility to transfer funds
+  /// @param recipient address of recipient
+  /// @param amount amount being transfered 
   function _transfer(address recipient, uint amount) private {
     require(amount > 0, "Account balance is 0");
     (bool sent, ) = payable(recipient).call{value: amount, gas: 2300}("");
